cd15074502b1f089120366a131265056317c646e
Tamnil Saito Junior <tamnil@gmail.com>
2020-02-10 05:53:16 -0300

    sync

--- cli/cli.js
+++ cli/cli.js
@@ -5,10 +5,10 @@
  *
  *
  */
-const util = require("util");
-const { execSync, exec, spawn } = require("child_process");
-const execPrommised = util.promisify(require("child_process").exec);
-const dict = require("../dict");
+const util = require("util"),
+    { execSync, exec, spawn } = require("child_process"),
+    execPrommised = util.promisify(require("child_process").exec),
+    dict = require("../dict");
 
 const argvIn = process.argv,
     absoluteFileName = argvIn[1],
@@ -21,7 +21,6 @@ const onlyArgsIn = argvIn.slice(2),
     onlyArgsOut = onlyArgsIn.join(" ");
 
 const commandType = command => {
-    const retVal = false;
     if (command.match(regexDk)) {
         return { type: "dk", cmd: "docker" };
     } else if (command.match(regexDkc)) {
@@ -30,37 +29,29 @@ const commandType = command => {
     return { type: "dk", cmd: "docker" };
 };
 
+const formatConsoleOut = type => data => console.log(data.toString()),
+    finishProcess = type => code => process.exit(code);
+
+const mySpawn = command => {
+    const sp = spawn(command, [], { shell: true });
+
+    sp.stdout.on("data", formatConsoleOut("stdout"));
+    sp.stderr.on("data", formatConsoleOut("stderr"));
+    process.stdin.on("data", data => sp.stdin.write(data));
+    sp.on("disconnect", finishProcess("diconnect"));
+    sp.on("error", finishProcess("error"));
+    sp.on("exit", finishProcess("exit"));
+    sp.on("data", finishProcess("data"));
+
+    // spawn end
+};
 const cli = () => {
     let command = commandType(absoluteFileName);
     let overrides = Object.keys(dict[command.type]);
-    const formatConsoleOut = data => console.log(data.toString());
 
     // spawn beg
-    const mySpawn = command => {
-        const sp = spawn(command, [], { shell: true });
-
-        sp.stdout.on("data", formatConsoleOut);
-        sp.stderr.on("data", formatConsoleOut);
-        process.stdin.on("data", data => {
-            sp.stdin.write(data);
-        });
-        sp.on("disconnect", function(code) {
-            process.exit(code);
-        });
-        sp.on("error", function(code) {
-            process.exit(code);
-        });
-
-        sp.on("exit", function(code) {
-            process.exit(code);
-        });
-
-        sp.on("data", function(code) {
-            process.exit(code);
-        });
-    // spawn end
-    };
     let operation = overrides.find(key => key === mainArg);
+
     if (operation) {
         console.log("override");
         let retCommand = dict[command.type][mainArg];
