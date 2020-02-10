eb0ba265ecdd0f1e0eee8e7f011b0ea37a1ae810
Tamnil Saito Junior <tamnil@gmail.com>
2020-02-10 06:32:50 -0300

    sync

--- cli/cli.js
+++ cli/cli.js
@@ -17,40 +17,41 @@ const argvIn = process.argv,
 const regexDk = /dk$|dk-cli.js$/,
     regexDkc = /dkc$|dkc-cli.js$/;
 
-const onlyArgsIn = argvIn.slice(2),
-    onlyArgsOut = onlyArgsIn.join(" ");
-
 const commandType = cmd =>
     cmd.match(regexDkc)
         ? { type: "dkc", cmd: "docker-compose" }
         : { type: "dk", cmd: "docker" };
 
+const onlyArgsIn = argvIn.slice(2),
+    onlyArgsOut = onlyArgsIn.join(" "),
+    command = commandType(absoluteFileName),
+    overrides = Object.keys(dict[command.type]);
+
 const formatConsoleOut = type => data => console.log(data.toString()),
     finishProcess = type => code => process.exit(code);
 
-const mySpawn = command => {
+const spawnCommand = command => {
     const sp = spawn(command, [], { shell: true });
 
+    //set stdio:
     sp.stdout.on("data", formatConsoleOut("stdout"));
     sp.stderr.on("data", formatConsoleOut("stderr"));
+
+    // process inputs
     process.stdin.on("data", data => sp.stdin.write(data));
-    sp.on("disconnect", finishProcess("diconnect"));
-    sp.on("error", finishProcess("error"));
-    sp.on("exit", finishProcess("exit"));
-    sp.on("data", finishProcess("data"));
-
-    // spawn end
+    // finish proccesses
+    ["disconnect", "error", "exit", "data"].map(el => {
+        sp.on(el, finishProcess(el));
+    });
 };
-const cli = () => {
-    let command = commandType(absoluteFileName);
-    let overrides = Object.keys(dict[command.type]);
 
-    // spawn beg
+
+const cli = () => {
     let operation = overrides.find(key => key === mainArg),
         overrideCmd = dict[command.type][mainArg],
         rawCmd = `${command.cmd} ${onlyArgsOut}`;
 
-    mySpawn(operation ? overrideCmd.out : rawCmd);
+    spawnCommand(operation ? overrideCmd.out : rawCmd);
 };
 
 module.exports = {
