3bbe8f9c6109b87ebb6d2e37ddc004ad4ea34602
Tamnil Saito Junior <tamnil@gmail.com>
2020-02-10 05:24:49 -0300

    sync

--- cli/cli.js
+++ cli/cli.js
@@ -30,40 +30,78 @@ const commandType = command => {
     return { type: "dk", cmd: "docker" };
 };
 
-
 const cli = () => {
     let command = commandType(absoluteFileName);
     let overrides = Object.keys(dict[command.type]);
 
+    // spawn beg
+    const mySpawn = command => {
+        const sp = spawn(command, [], { shell: true });
+
+        sp.stdout.on("data", data => {
+            console.log(data.toString());
+        });
+        sp.stderr.on("data", data => {
+            console.log(data.toString());
+        });
+        process.stdin.on("data", data => {
+            console.log(data.toString());
+
+            sp.stdin.write(data);
+        });
+        sp.on("disconnect", function(code) {
+            console.log("child process exited with code disconnect" + code);
+            process.exit(code);
+        });
+        sp.on("error", function(code) {
+            console.log("erro: " + code, "ls", ls.stdout.toString());
+            process.exit(code);
+        });
+
+        // doesn't fire
+        sp.on("exit", function(code) {
+            console.log("child process exited with code exit " + code);
+            process.exit(code);
+        });
+
+        // doesn't fire
+        sp.on("data", function(code) {
+            console.log("data direct" + code);
+            process.exit(code);
+        });
+    // spawn end
+    };
     let operation = overrides.find(key => key === mainArg);
     if (operation) {
         console.log("override");
         let retCommand = dict[command.type][mainArg];
-        execPrommised(retCommand.out)
-            .then(res => {
-                console.log("res", res);
-                if (res.stderr) {
-                    console.log(res.stderr.toString());
-                } else {
-                    console.log(res.stdout.toString());
-                }
-            })
-            .catch(err => {
-                console.log("error", err.stderr);
-            });
+        mySpawn(retCommand.out);
+    // .then(res => {
+    //     console.log("res", res);
+    //     if (res.stderr) {
+    //         console.log(res.stderr.toString());
+    //     } else {
+    //         console.log(res.stdout.toString());
+    //     }
+    // })
+    // .catch(err => {
+    //     console.log("error", err.stderr);
+    // });
     } else {
         console.log("native");
-        execPrommised(`${command.cmd} ${onlyArgsOut}`)
-            .then(res => {
-                if (res.stderr) {
-                    console.log(res.stderr.toString());
-                } else {
-                    console.log(res.stdout.toString());
-                }
-            })
-            .catch(err => {
-                console.log(err.stderr.toString());
-            });
+        mySpawn(`${command.cmd} ${onlyArgsOut}`)
+     // console.log('fasdf')
+        // mySpawn('docker image ')
+            // .then(res => {
+            //     if (res.stderr) {
+            //         console.log(res.stderr.toString());
+            //     } else {
+            //         console.log(res.stdout.toString());
+            //     }
+            // })
+            // .catch(err => {
+            //     console.log(err.stderr.toString());
+            // });
     }
 };
 
