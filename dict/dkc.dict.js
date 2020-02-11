e7ecaf4b16774aa9e312cd3815c9d3469469cbc1
Tamnil Saito Junior <tamnil@gmail.com>
2020-02-10 21:28:44 -0300

    more docker compose commands

--- dict/dkc.dict.js
+++ dict/dkc.dict.js
@@ -10,9 +10,38 @@ const dkc = {
         descr: "docker-compose up build"
     },
     u: {
-        out: "docker-compose up --build",
-        descr: "docker-compose up build"
-    }
+        out: "docker-compose up",
+        descr: "docker-compose up"
+     },
+    d: {
+        out: "docker-compose down",
+        descr: "docker-compose down"
+     },
+    b: {
+        out: "docker-compose build",
+        descr: "docker-compose build"
+     },
+
+    ev: {
+        out: "docker-compose events",
+        descr: "docker-compose events"
+     },
+    res: {
+        out: "docker-compose restart",
+        descr: "docker-compose restart"
+     },
+    p: {
+        out: "docker-compose pause",
+        descr: "docker-compose pause"
+     },
+
+    unp: {
+        out: "docker-compose unpause",
+        descr: "docker-compose unpause"
+    },
+    "im": { out: "docker-compose images", desc: " " },
+    "l": { out: "docker-compose logs", desc: " " },
+    // "": { out: "", desc: " " },
     // "": { out: "", desc: " " },
     // "": { out: "", desc: " " },
     // "": { out: "", desc: " " },
